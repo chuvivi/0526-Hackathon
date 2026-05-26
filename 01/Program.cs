9dcdbe3320a8bd2ded2f87d675e2e7f26b258df1
ViviChu <chuviviwei@gmail.com>
2026-05-26 11:08:38 +0800

    加入專案檔案。

---
+++ 01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個大於 90 的整數 : ");
+            int max = int.Parse(Console.ReadLine());
+            for ( int i = 1; i <= max; i++ )
+            {
+                if ( i % 3 == 0 && i % 5 == 0 )
+                {
+                    Console.WriteLine("Dann");
+                }
+                else if ( i % 3 == 0)
+                {
+                    Console.WriteLine("Build");
+                }
+                else if ( i % 5 == 0)
+                {
+                    Console.WriteLine("School");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
