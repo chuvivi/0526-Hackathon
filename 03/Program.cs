37c8f400504f4e58aec9eaaa2e6c331f083d374b
ViviChu <chuviviwei@gmail.com>
2026-05-26 14:16:01 +0800

    新增03

---
+++ 03/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請以英文輸入一句話：");
+            string[] input = Console.ReadLine().ToLower().Split(' ');
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            foreach (string word in input)
+            {
+                if(wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            foreach(var item in wordCount)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
